e889152059d736d9e1e2bcff5bd4cfd55a97a1f3
italorbr <italorosa.profissional@gmail.com>
2023-08-09 20:10:40 -0300

    meu nonagesimo commit

---
+++ exemplo02.php
@@ -0,0 +1,7 @@
+<?php 
+    $valor1 = 50;
+    $valor2 = 30;
+    $resp = ($valor1*$valor2);
+
+    echo("A multiplicação é: " . $resp);
+?>
---
+++ exemplo03.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo ("criando mais um projeto <br>");
+
+    $nota1 = 6.5;
+    $nota2 = 8.5;
+    $trabalho = 9.5;
+
+    $media = ($nota1 + $nota2 + $trabalho)/3;
+
+?>
